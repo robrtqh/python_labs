0a83d9e092b571ef108ffc513563b6b5c55f2bde
robrtqh <enserdyuk@gmail.com>
2025-10-02 20:43:03 +0300

    Add lab01 files and screenshots

---
+++ src/lab02/arrays.py
@@ -0,0 +1,35 @@
+def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
+    try:
+        return tuple([min(nums), max(nums)])
+    except ValueError:
+        return 'ValueError'
+
+print('min_max')
+print(min_max([1, 2, 3, 4, 5]))
+print(min_max([42]))
+print(min_max([-5, -2, -9]))
+print(min_max([]))
+print(min_max([1.5, 2, 2.0, -3.1]))
+
+def unique_sorted(nums: list[float | int]) -> list[float | int]:
+    return sorted(set(nums))
+
+print('unique_sorted')
+print(unique_sorted([3, 1, 2, 1, 3]))
+print(unique_sorted([]))
+print(unique_sorted([-1, -1, 0, 2, 2]))
+print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+
+def flatten(mat: list[list | tuple]) -> list:
+    result = []
+    for i in mat:
+        if not isinstance(i, (list, tuple)):
+            return "TypeError"
+        result.extend(i)
+    return result
+
+print('flatten')
+print(flatten([[1, 2], [3, 4]]))
+print(flatten(([1, 2], (3, 4, 5))))
+print(flatten([[1], [], [2, 3]]))
+print(flatten([[1, 2], "ab"]))
---
+++ src/lab02/matrix.py
@@ -0,0 +1,42 @@
+def transpose(mat: list[list[float | int]]) -> list[list]:
+    if not mat:
+        return []
+    num = len(mat[0])
+    if any(len(row) != num for row in mat):
+        return "ValueError"
+    return [[mat[i][j] for i in range(len(mat))] for j in range(num)]
+
+print('transpose')
+print(transpose([[1, 2], [3, 4]]))
+print(transpose([[1], [2], [3]]))
+print(transpose([[1, 2], [3, 4]]))
+print(transpose([]))
+print(transpose([[1, 2], [3]]))
+
+def row_sums(mat: list[list[float | int]]) -> list[float]:
+    if not mat:
+        return []
+    if mat and any(len(row) != len(mat[0]) for row in mat):
+        return "ValueError"
+    return [sum(row) for row in mat]
+
+print('row_sum')
+print(row_sums([[1, 2, 3], [4, 5, 6]]))
+print(row_sums([[-1, 1], [10, -10]]))
+print(row_sums([[0, 0], [0, 0]]))
+print(row_sums([[1, 2], [3]]))
+
+
+def col_sums(mat: list[list[float | int]]) -> list[float]:
+    if not mat:
+        return []
+    num = len(mat[0])
+    if any(len(row) != num for row in mat):
+        return "ValueError"
+    return [sum(mat[i][j] for i in range(len(mat))) for j in range(num)]
+
+print('col_sums')
+print(col_sums([[1, 2, 3], [4, 5, 6]]))
+print(col_sums([[-1, 1], [10, -10]]))
+print(col_sums([[0, 0], [0, 0]]))
+print(col_sums([[1, 2], [3]]))
